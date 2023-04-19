598104a61f5b1c9e8594f9987f6aa216c23a5189
Martin Křivda <martin.krivda@kobchocen.cz>
2023-04-19 20:17:38 +0200

    Docker deployment

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,6 +10,26 @@ async function main() {
     },
   });
   console.log({ orienteering });
+  const eventName = process.env.EVENT_NAME;
+  const date = process.env.DATE && new Date(process.env.DATE);
+  const organizer = process.env.ORGANIZER && process.env.ORGANIZER;
+  const zeroTime = process.env.ZERO_TIME && new Date(process.env.ZERO_TIME);
+  const isRelay = process.env.RELAY && Boolean(Number(process.env.RELAY));
+  const location = process.env.LOCATION && process.env.LOCATION;
+
+  const defaultEvent = await prisma.event.create({
+    data: {
+      name: eventName,
+      date: date,
+      organizer: organizer,
+      zeroTime: zeroTime,
+      relay: isRelay,
+      location: location,
+      sportId: 1,
+      published: true,
+    },
+  });
+  console.log({ defaultEvent });
 }
 main()
   .then(async () => {
