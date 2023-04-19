364e53749476ce5234b8aa75ceac908e36a92d31
Martin Křivda <martin.krivda@kobchocen.cz>
2023-04-19 21:28:15 +0200

    feat. seed default event

--- prisma/seed.js
+++ prisma/seed.js
@@ -9,27 +9,36 @@ async function main() {
       name: 'Orienteering',
     },
   });
-  console.log({ orienteering });
   const eventName = process.env.EVENT_NAME;
-  const date = process.env.DATE && new Date(process.env.DATE);
-  const organizer = process.env.ORGANIZER && process.env.ORGANIZER;
-  const zeroTime = process.env.ZERO_TIME && new Date(process.env.ZERO_TIME);
-  const isRelay = process.env.RELAY && Boolean(Number(process.env.RELAY));
-  const location = process.env.LOCATION && process.env.LOCATION;
+  if (eventName) {
+    console.log({ orienteering });
+    const dbEventResponse = await prisma.event.findFirst({
+      where: {
+        name: eventName,
+      },
+    });
+    if (!dbEventResponse) {
+      const date = process.env.DATE && new Date(process.env.DATE);
+      const organizer = process.env.ORGANIZER && process.env.ORGANIZER;
+      const zeroTime = process.env.ZERO_TIME && new Date(process.env.ZERO_TIME);
+      const isRelay = process.env.RELAY && Boolean(Number(process.env.RELAY));
+      const location = process.env.LOCATION && process.env.LOCATION;
 
-  const defaultEvent = await prisma.event.create({
-    data: {
-      name: eventName,
-      date: date,
-      organizer: organizer,
-      zeroTime: zeroTime,
-      relay: isRelay,
-      location: location,
-      sportId: 1,
-      published: true,
-    },
-  });
-  console.log({ defaultEvent });
+      const defaultEvent = await prisma.event.create({
+        data: {
+          name: eventName,
+          date: date,
+          organizer: organizer,
+          zeroTime: zeroTime,
+          relay: isRelay,
+          location: location,
+          sportId: 1,
+          published: true,
+        },
+      });
+      console.log({ defaultEvent });
+    }
+  }
 }
 main()
   .then(async () => {
