f8bf6da740079820537e1496610948fbf484cbe9
Martin Křivda <martin.krivda@kobchocen.cz>
2025-02-14 17:58:30 +0100

    remove temp mapycz leaflet

--- oricloud-console/src/pages/contact/ContactPage.jsx
+++ oricloud-console/src/pages/contact/ContactPage.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { FloatingBadge } from '../../organisms';
 import { EventPageLayout } from '../../templates';
-import { Map } from 'react-mapycz-leaflet';
 
 export const ContactPage = () => {
   const { t } = useTranslation();
@@ -106,10 +105,6 @@ export const ContactPage = () => {
             </Link>{' '}
             so we can squash it faster than you reach the next control point.
           </p>
-          <Map
-            apiKey="tPVxGxmTRbdoc4vDVIIvTTIMRmVjdJgcwv9U_Hqhekk"
-            center={{ lat: 50.0755, lng: 14.4378 }}
-          />
         </section>
       </div>
     </EventPageLayout>
